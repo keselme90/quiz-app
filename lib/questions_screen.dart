2e72560fbff069a429cf9c9c199788a33d3cf1d0
keselme <keselme90@gmail.com>
2023-08-05 17:49:25 +0300

    Update: integrate the citom buttons and questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 class QuestionsScreen extends StatefulWidget {
   
   const QuestionsScreen({super.key});
@@ -12,19 +13,43 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  var currentQuestionIndex = 0;
+
+  void answerQuestion () {
+    setState(() {
+      currentQuestionIndex ++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-   return SizedBox( // An alternative way to center the elemnts on the screen
-    width: double.infinity, // Take as much width as possible
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text("The question..."),
-        const SizedBox(height: 30,),
-        ElevatedButton(onPressed: () {}, child: const Text("Answer 1"))
-      ],
-     ),
-   );
+
+    final currentQuestion = questions[currentQuestionIndex]; 
+
+    return Container(
+      margin: const EdgeInsets.all(40),
+      child: SizedBox( // An alternative way to center the elemnts on the screen
+        width: double.infinity, // Take as much width as possible
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch, // Items will strexch out to be as wide as possible
+          children: [
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(
+                color: Colors.white
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30,),
+            ...currentQuestion.getShuffledAnswers().map((item) {
+              return AnswerButton(text: item, onPress: answerQuestion);
+            })
+          ],
+        ),
+      ),
+    );
   }
 
 }

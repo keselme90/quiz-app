50ea563f9ac66098f9464454139335045d50e911
keselme <keselme90@gmail.com>
2023-08-05 17:48:54 +0300

    Add: method to shufle answetrs

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
   final List<String> answers;
 
   const QuizQuestion(this.question, this.answers);
+
+  List <String> getShuffledAnswers() {
+    final List<String> shuffled = List.of(answers);
+    shuffled.shuffle();
+    return shuffled;
+  }
 }

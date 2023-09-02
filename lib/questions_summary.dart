8e78c541af2421daa0889385a02cdcb6ae70ade9
keselme <keselme90@gmail.com>
2023-09-02 17:53:53 +0300

    Update: use QuizQuestionIndex widget

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/quiz_question_number.dart';
 
 class QuestionsSummary extends StatelessWidget {
   
@@ -9,22 +11,50 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    bool isCorrectAnswer(String userAnswer, String correctAnswer) {
+      return userAnswer == correctAnswer;
+    }
+
     return SizedBox(
       height: 300,
       child: SingleChildScrollView( // Takes a single child and makes it scrollable
         child: Column(
           children: summaryData.map((data) {
             return Row(
+              crossAxisAlignment: CrossAxisAlignment.start, // We add this to make sure that the elemnts will be aligned to the top of the row
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
+                QuizQuestionNumber(
+                  questionIndex: ((data['question_index'] as int) + 1).toString(),
+                  isCorrectAnswer: isCorrectAnswer(data['user_answer'] as String, data['correct_answer'] as String)
+                ),
+                const SizedBox(width: 20,),
                 Expanded( // Without Expanced widget Column takes an infinite amount of width, even if it goes beyond the screen boundaries. 
                           // Allows the child to take all avaiable width along the main axis. The widget passed as a child to Exapanded cannot take more width than the Expaned's parent widget.
-                  child: Column(children: [
-                    Text(data['question'] as String),
-                    const SizedBox(height: 5,),
-                    Text(data['user_answer'] as String),
-                    Text(data['correct_answer'] as String)
-                  ],),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // We add this to make sure that all text is aligned to the left
+                    children: [
+                      Text(
+                        data['question'] as String,
+                        style: GoogleFonts.lato(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      const SizedBox(height: 5,),
+                      Text(
+                        data['user_answer'] as String,
+                        style: const TextStyle(
+                          color: Colors.amber
+                        )),
+                      Text(
+                        data['correct_answer'] as String,
+                        style: const TextStyle(
+                          color: Colors.teal
+                        ),)
+                    ],
+                  ),
                 )
               ],
             );
